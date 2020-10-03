bfccdb1e27ad2b3d51f8ec6600ee2939b4a3fa36
khanhnguyenk8 <khanhnguyenk8@gmail.com>
2020-10-03 13:49:34 +0700

    Phong_p1

--- DAL/phongDAL.cs
+++ DAL/phongDAL.cs
@@ -10,6 +10,21 @@ namespace DAL
 {
    public class phongDAL
     {
+        public DataTable hienthithongtinphong()
+        {
+            SqlConnection cnn = ketnoi.Get();
+            // Khai báo và khởi tạo đối tượng Command, truyền vào tên thủ tục tương ứng
+            SqlCommand cmd = new SqlCommand("hienthiphong", cnn);
+            // Khai báo kiểu thực thi là Thực thi thủ tục
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cnn;
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cnn.Close();
+            return dt;
+        }
 
     }
 }
--- DTO/phongDTO.cs
+++ DTO/phongDTO.cs
@@ -8,5 +8,32 @@ namespace DTO
 {
    public class phongDTO
     {
+        private float _donGia;
+        private string _maPhong;
+        private bool _tinhTrang;
+        private string _loaiPhong;
+
+
+        public string maPhong
+        {
+            get { return _maPhong; }
+            set { _maPhong = value; }
+        }
+        public bool tinhTrang
+        {
+            get { return _tinhTrang; }
+            set { _tinhTrang = value; }
+        }
+        public string loaiPhong
+        {
+            get { return _loaiPhong; }
+            set { _loaiPhong = value; }
+        }
+        public float donGia
+        {
+            get { return _donGia; }
+            set { _donGia = value; }
+        }
+
     }
 }
