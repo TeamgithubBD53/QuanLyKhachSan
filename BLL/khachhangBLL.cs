2cc7de35a2ccf15e3e91aae7d88304d497b85149
vanta3331 <vanta06123@gmail.com>
2020-10-06 19:50:12 +0700

    KhachhangBLL

--- BLL/khachhangBLL.cs
+++ BLL/khachhangBLL.cs
@@ -10,6 +10,27 @@ namespace BLL
 {
    public class khachhangBLL
     {
-      
+
+        khachhangDAL khDAL = new khachhangDAL();
+        public DataTable hienthikhachhang()
+        {
+            return khDAL.hienthikhachhang();
+        }
+
+        public bool them_khachhang(string tenKhachHang, DateTime ngaySinh, bool gioiTinh, string chungMinhNhanDan, string diaChi, string soDienThoai, string quocTich)
+        {
+            return khDAL.them_khachhang(tenKhachHang, ngaySinh, gioiTinh, chungMinhNhanDan, diaChi, soDienThoai, quocTich);
+        }
+
+        public void xoa_khachhang(int maKhachHang)
+        {
+            khDAL.xoa_khachhang(maKhachHang);
+        }
+
+        public void sua_khachhang(string tenKhachHang, DateTime ngaySinh, bool gioiTinh, string chungMinhNhanDan, string diaChi, string soDienThoai, string quocTich, int maKhachHang)
+        {
+
+            khDAL.sua_khachhang(tenKhachHang, ngaySinh, gioiTinh, chungMinhNhanDan, diaChi, soDienThoai, quocTich, maKhachHang);
+        }
     }
 }
