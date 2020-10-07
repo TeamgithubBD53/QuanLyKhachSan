e29fee080c22b62b1c22a638894ecad1db9d6413
vanta3331 <vanta06123@gmail.com>
2020-10-07 14:40:45 +0700

    them va hien thi kh

--- quanlykhachsan/frmkhachhang.cs
+++ quanlykhachsan/frmkhachhang.cs
@@ -17,6 +17,54 @@ namespace quanlykhachsan
         {
             InitializeComponent();
         }
+        int temp;
+        private void hienthi()
+        {
+            khachhangBLL pbll = new khachhangBLL();
+            dataGridView1.DataSource = pbll.hienthikhachhang();
+        }
+        private void trangthai(bool t)
+        {
+            // txtmakhachhang.Enabled = t;
+            txttenkh.Enabled = t;
+            txtcmt.Enabled = t;
+            txtsdt.Enabled = t;
+            txtdiachi.Enabled = t;
+            dtngaysinh.Enabled = t;
+            cbquoctich.Enabled = t;
+            cbgiotinhkh.Enabled = t;
+        }
+        private void frm_load(object sender, EventArgs e)
+        {
+            hienthi();
+            trangthai(false);
+        }
+        
 
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dataGridView1.Rows[e.RowIndex];
+            txtmakhachhang.Text = row.Cells["maKhachHang"].Value.ToString();
+            txttenkh.Text = row.Cells["tenKhachHang"].Value.ToString();
+            txtcmt.Text = row.Cells["chungMinhNhanDan"].Value.ToString();
+            txtsdt.Text = row.Cells["soDienThoai"].Value.ToString();
+            txtdiachi.Text = row.Cells["diaChi"].Value.ToString();
+            cbquoctich.Text = row.Cells["quocTich"].Value.ToString();
+            cbgiotinhkh.Text = row.Cells["gioiTinh"].Value.ToString();
+            dtngaysinh.Value = Convert.ToDateTime(row.Cells["ngaySinh"].Value.ToString());
+        }
+        private bool travegioitinh()
+        {
+            if (cbgiotinhkh.Text == "Nam")
+                return true;
+            else
+                return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temp = 1;
+            trangthai(true);
+        }
     }
 }
