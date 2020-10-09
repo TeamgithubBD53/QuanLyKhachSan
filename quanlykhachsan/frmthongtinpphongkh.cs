1d290e1fad19a4f9bfad66c859d8eb7261f7bf72
ThanhThuy2908 <Thanhthuybt2908@gmail.com>
2020-10-09 22:26:58 +0700

    thongtinphongKH

--- quanlykhachsan/frmthongtinpphongkh.cs
+++ quanlykhachsan/frmthongtinpphongkh.cs
@@ -18,5 +18,22 @@ namespace quanlykhachsan
             InitializeComponent();
         }
 
+        private void frm_load(object sender, EventArgs e)
+        {
+            phongBLL pbll = new phongBLL();
+            dataGridView1.DataSource = pbll.hienthithongtinphong();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmdangnhap frm = new frmdangnhap();
+            frm.Show();
+        }
+
+        private void thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
